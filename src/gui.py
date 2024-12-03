1ebc3889e6bee5a715aff231b98de8ab891df59d
wyatthoho <wyatthoho@gmail.com>
2024-12-03 17:32:03 +0800

    refactor: extract main function

--- src/gui.py
+++ src/gui.py
@@ -5,12 +5,12 @@ from tkinter import font
 from math import floor, log10
 
 
-def open_dir():
+def open_dir(dir_variable: tk.StringVar):
     dir_name = filedialog.askdirectory(title='Choose the directory')
     dir_variable.set(dir_name)
 
 
-def config_replace():
+def config_replace(replace_state: tk.IntVar, find_entry: tk.Entry, replace_entry: tk.Entry):
     if replace_state.get():
         find_entry.config(state='normal')
         replace_entry.config(state='normal')
@@ -19,7 +19,7 @@ def config_replace():
         replace_entry.config(state='disabled')
 
 
-def config_order():
+def config_order(order_state: tk.IntVar, prefix_radio1: tk.Radiobutton, prefix_radio2: tk.Radiobutton, prefix_radio3: tk.Radiobutton, up_btn: tk.Button, dw_btn: tk.Button):
     if order_state.get():
         prefix_radio1.config(state='normal')
         prefix_radio2.config(state='normal')
@@ -34,7 +34,7 @@ def config_order():
         dw_btn['state'] = tk.DISABLED
 
 
-def config_suffix():
+def config_suffix(suffix_state: tk.IntVar, suffix_entry: tk.Entry):
     if suffix_state.get():
         suffix_entry.config(state='normal')
     else:
@@ -47,15 +47,15 @@ def clean_prefix(names: list):
         name_separated = name.partition('_')
         if name_separated[0].isdigit():
             name = name_separated[-1]
-    
+
         name_separated = name.partition('-')
         if name_separated[0].isdigit():
             name = name_separated[-1]
-    
+
         name_separated = name.partition(' ')
         if name_separated[0].isdigit():
             name = name_separated[-1]
-    
+
         new_names.append(name)
     return new_names
 
@@ -66,7 +66,7 @@ def refresh_list_box(list_box: tk.Listbox, names: list):
         list_box.insert(idx, name)
 
 
-def read_names():
+def read_names(dir_variable: tk.StringVar, apply_value: tk.IntVar, list_box_read: tk.Listbox):
     tgt_dir_name = dir_variable.get()
     dir_names = os.listdir(tgt_dir_name)
 
@@ -78,10 +78,10 @@ def read_names():
     refresh_list_box(list_box_read, names)
 
 
-def move_name(inc: int):
+def move_name(inc: int, list_box_read: tk.Listbox):
     moved = False
     names = list(list_box_read.get(0, tk.END))
-    names_dict = {idx:name for idx, name in enumerate(names)}
+    names_dict = {idx: name for idx, name in enumerate(names)}
 
     if inc < 0:
         id_boundary = 0
@@ -102,18 +102,18 @@ def move_name(inc: int):
         list_box_read.select_set(id_next)
 
 
-def replace_names(names: list):
+def replace_names(names: list, find_entry: tk.Entry, replace_entry: tk.Entry):
     find_str = find_entry.get()
     replace_str = replace_entry.get()
     return [name.replace(find_str, replace_str) for name in names]
 
 
-def add_suffix(names: list):
+def add_suffix(names: list, suffix_entry: tk.Entry):
     suffix = suffix_entry.get()
-    return [''.join([_str + suffix if idx==0 else _str for idx, _str in enumerate(name.rpartition('.'))]) for name in names]
+    return [''.join([_str + suffix if idx == 0 else _str for idx, _str in enumerate(name.rpartition('.'))]) for name in names]
 
 
-def reorder_names(names: list):
+def reorder_names(names: list, radio_value: tk.IntVar):
     names = clean_prefix(names)
     if radio_value.get() == 1:
         sep = '_'
@@ -135,24 +135,24 @@ def check_repeated(list_box: tk.Listbox, names: list):
         for idx2, name2 in enumerate(names[idx1+1:]):
             if name2 == name1:
                 name_repeated = True
-                list_box.itemconfig(idx1, {'bg':'red'})
-                list_box.itemconfig(idx1+idx2+1, {'bg':'red'})
+                list_box.itemconfig(idx1, {'bg': 'red'})
+                list_box.itemconfig(idx1+idx2+1, {'bg': 'red'})
 
 
-def preview_names():
+def preview_names(list_box_read: tk.Listbox, list_box_preview: tk.Listbox, replace_state: tk.IntVar, suffix_state: tk.IntVar, order_state: tk.IntVar, find_entry: tk.Entry, replace_entry: tk.Entry, suffix_entry: tk.Entry, radio_value: tk.IntVar):
     names = list(list_box_read.get(0, tk.END))
     if replace_state.get():
-        names = replace_names(names)
+        names = replace_names(names, find_entry, replace_entry)
     if suffix_state.get():
-        names = add_suffix(names)
+        names = add_suffix(names, suffix_entry)
     if order_state.get():
-        names = reorder_names(names)
+        names = reorder_names(names, radio_value)
 
     refresh_list_box(list_box_preview, names)
     check_repeated(list_box_preview, names)
 
 
-def rename():
+def rename(dir_variable: tk.StringVar, list_box_read: tk.Listbox, list_box_preview: tk.Listbox):
     tgt_dir_name = dir_variable.get()
     names_src = list(list_box_read.get(0, tk.END))
     names_dst = list(list_box_preview.get(0, tk.END))
@@ -174,7 +174,7 @@ def rename():
         list_box_preview.delete(0, tk.END)
 
 
-if __name__ == '__main__':
+def main():
     file_dir = os.path.dirname(__file__)
     par_dir = os.path.abspath(os.path.join(file_dir, os.path.pardir))
     logo_path = os.path.join(par_dir, 'image', 'w.ico')
@@ -186,12 +186,12 @@ if __name__ == '__main__':
 
     label_font = font.Font(family='Helvetica', size=10)
     btn_font = font.Font(family='Helvetica', size=10)
-    
+
     # frame up
     frame_up = tk.LabelFrame(root, text='Choose the directory')
     frame_up.grid(row=0, column=0, padx=4, pady=4, ipadx=1, ipady=1, sticky=tk.W)
     frame_up['font'] = label_font
-    
+
     frame_up_up = tk.Frame(frame_up)
     frame_up_up.grid(row=0, column=0)
 
@@ -208,19 +208,19 @@ if __name__ == '__main__':
     apply_label = tk.Label(frame_up_dw, text='Apply to:')
     apply_label.grid(row=0, column=0, padx=4)
 
-    apply_value = tk.IntVar() 
+    apply_value = tk.IntVar()
     file_radio = tk.Radiobutton(frame_up_dw, text='Files', variable=apply_value, value=1)
     file_radio.grid(row=0, column=1)
-    
+
     folder_radio = tk.Radiobutton(frame_up_dw, text='Folders', variable=apply_value, value=2)
     folder_radio.grid(row=0, column=2)
     apply_value.set(1)
 
-    choose_btn = tk.Button(frame_up_right, text='Choose', command=open_dir, width=6)
+    choose_btn = tk.Button(frame_up_right, text='Choose', command=lambda: open_dir(dir_variable), width=6)
     choose_btn.grid(row=0, column=0, padx=4, pady=4, ipadx=1, ipady=1)
     choose_btn['font'] = btn_font
 
-    read_btn = tk.Button(frame_up_right, text='Read', command=read_names, width=6)
+    read_btn = tk.Button(frame_up_right, text='Read', command=lambda: read_names(dir_variable, apply_value, list_box_read), width=6)
     read_btn.grid(row=1, column=0, padx=4, pady=4, ipadx=1, ipady=1)
     read_btn['font'] = btn_font
 
@@ -237,52 +237,52 @@ if __name__ == '__main__':
 
     frame_mid_dw = tk.Frame(frame_mid)
     frame_mid_dw.grid(row=2, column=0, sticky=tk.W)
-    
+
     replace_state = tk.IntVar()
-    replace_check_btn = tk.Checkbutton(frame_mid_up, text='Replace text', command=config_replace, variable=replace_state, onvalue=1, offvalue=0)
+    replace_check_btn = tk.Checkbutton(frame_mid_up, text='Replace text', command=lambda: config_replace(replace_state, find_entry, replace_entry), variable=replace_state, onvalue=1, offvalue=0)
     replace_check_btn.grid(row=0, column=0, padx=4, pady=4)
 
     find_label = tk.Label(frame_mid_up, text='Find:')
     find_label.grid(row=0, column=1, padx=4, pady=4)
-    
+
     find_entry = tk.Entry(frame_mid_up, width=13)
     find_entry.grid(row=0, column=2, padx=4, pady=4)
     find_entry.config(state='disabled')
-    
+
     replace_label = tk.Label(frame_mid_up, text='Replace:')
     replace_label.grid(row=0, column=3, padx=4, pady=4)
-    
+
     replace_entry = tk.Entry(frame_mid_up, width=13)
     replace_entry.grid(row=0, column=4, padx=4, pady=4)
     replace_entry.config(state='disabled')
 
     suffix_state = tk.IntVar()
-    suffix_sheck_btn = tk.Checkbutton(frame_mid_mid, text='Add suffix', command=config_suffix, variable=suffix_state, onvalue=1, offvalue=0)
+    suffix_sheck_btn = tk.Checkbutton(frame_mid_mid, text='Add suffix', command=lambda: config_suffix(suffix_state, suffix_entry), variable=suffix_state, onvalue=1, offvalue=0)
     suffix_sheck_btn.grid(row=0, column=0, padx=4, pady=4)
 
     suffix_label = tk.Label(frame_mid_mid, text='Suffix:')
     suffix_label.grid(row=0, column=1, padx=4, pady=4)
-    
+
     suffix_entry = tk.Entry(frame_mid_mid, width=13)
     suffix_entry.grid(row=0, column=2, padx=4, pady=4)
     suffix_entry.config(state='disabled')
 
     order_state = tk.IntVar()
-    order_check_btn = tk.Checkbutton(frame_mid_dw, text='Make an order', command=config_order, variable=order_state, onvalue=1, offvalue=0)
+    order_check_btn = tk.Checkbutton(frame_mid_dw, text='Make an order', command=lambda: config_order(order_state, prefix_radio1, prefix_radio2, prefix_radio3, up_btn, dw_btn), variable=order_state, onvalue=1, offvalue=0)
     order_check_btn.grid(row=1, column=0, padx=4, pady=4)
 
     sep_label = tk.Label(frame_mid_dw, text='Sep:')
     sep_label.grid(row=1, column=1, padx=4, pady=4)
 
-    radio_value = tk.IntVar() 
+    radio_value = tk.IntVar()
     prefix_radio1 = tk.Radiobutton(frame_mid_dw, text='_', variable=radio_value, value=1)
     prefix_radio1.grid(row=1, column=2, padx=4, pady=4)
     prefix_radio1.config(state='disabled')
-    
+
     prefix_radio2 = tk.Radiobutton(frame_mid_dw, text='-', variable=radio_value, value=2)
     prefix_radio2.grid(row=1, column=3, padx=4, pady=4)
     prefix_radio2.config(state='disabled')
-    
+
     prefix_radio3 = tk.Radiobutton(frame_mid_dw, text='space', variable=radio_value, value=3)
     prefix_radio3.grid(row=1, column=4, padx=4, pady=4)
     prefix_radio3.config(state='disabled')
@@ -292,41 +292,41 @@ if __name__ == '__main__':
     frame_dw = tk.LabelFrame(root, text='Name list')
     frame_dw.grid(row=2, column=0, padx=4, pady=4, ipadx=1, ipady=1, sticky=tk.W)
     frame_dw['font'] = label_font
-    
+
     frame_dw_left = tk.Frame(frame_dw)
     frame_dw_left.grid(row=0, column=0)
 
     scrollbar_read_x = tk.Scrollbar(frame_dw_left, orient=tk.HORIZONTAL)
     scrollbar_read_y = tk.Scrollbar(frame_dw_left, orient=tk.VERTICAL)
-    
+
     scrollbar_read_x.grid(row=1, column=0, sticky=tk.EW)
     scrollbar_read_y.grid(row=0, column=1, sticky=tk.NS)
 
     list_box_read = tk.Listbox(frame_dw_left, width=22, xscrollcommand=scrollbar_read_x.set, yscrollcommand=scrollbar_read_y.set)
     list_box_read.grid(row=0, column=0, padx=4, pady=4)
-    
+
     scrollbar_read_x.config(command=list_box_read.xview)
     scrollbar_read_y.config(command=list_box_read.yview)
 
     scrollbar_preview_x = tk.Scrollbar(frame_dw_left, orient=tk.HORIZONTAL)
     scrollbar_preview_y = tk.Scrollbar(frame_dw_left, orient=tk.VERTICAL)
-    
+
     scrollbar_preview_x.grid(row=1, column=2, sticky=tk.EW)
     scrollbar_preview_y.grid(row=0, column=3, sticky=tk.NS)
 
     list_box_preview = tk.Listbox(frame_dw_left, width=22, xscrollcommand=scrollbar_preview_x.set, yscrollcommand=scrollbar_preview_y.set)
     list_box_preview.grid(row=0, column=2, padx=4, pady=4)
-    
+
     scrollbar_preview_x.config(command=list_box_preview.xview)
     scrollbar_preview_y.config(command=list_box_preview.yview)
 
     frame_dw_right = tk.Frame(frame_dw)
     frame_dw_right.grid(row=0, column=1)
 
-    up_btn = tk.Button(frame_dw_right, text='Up', command=lambda: move_name(-1), width=6)
-    dw_btn = tk.Button(frame_dw_right, text='Down', command=lambda: move_name(1), width=6)
-    pre_btn = tk.Button(frame_dw_right, text='Preview', command=preview_names, width=6)
-    run_btn = tk.Button(frame_dw_right, text='Run',command=rename, width=6)
+    up_btn = tk.Button(frame_dw_right, text='Up', command=lambda: move_name(-1, list_box_read), width=6)
+    dw_btn = tk.Button(frame_dw_right, text='Down', command=lambda: move_name(1, list_box_read), width=6)
+    pre_btn = tk.Button(frame_dw_right, text='Preview', command=lambda: preview_names(list_box_read, list_box_preview, replace_state, suffix_state, order_state, find_entry, replace_entry, suffix_entry, radio_value), width=6)
+    run_btn = tk.Button(frame_dw_right, text='Run', command=lambda: rename(dir_variable, list_box_read, list_box_preview), width=6)
     up_btn['state'] = tk.DISABLED
     dw_btn['state'] = tk.DISABLED
 
@@ -335,4 +335,7 @@ if __name__ == '__main__':
         widget['font'] = btn_font
 
     root.mainloop()
-    
+
+
+if __name__ == '__main__':
+    main()
