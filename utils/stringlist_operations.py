89332261fabac9d2686abf8a537e0c87e70564a7
wyatthoho <wyatthoho@gmail.com>
2025-04-23 21:07:27 +0800

    refactor: modify type hints

--- utils/stringlist_operations.py
+++ utils/stringlist_operations.py
@@ -1,18 +1,21 @@
 from math import floor, log10
 
+type Names = list[str]
+type Indices = list[int]
 
-def replace_names(names: list[str], find_str: str, replace_str: str) -> list[str]:
+
+def replace_names(names: Names, find_str: str, replace_str: str) -> Names:
     return [name.replace(find_str, replace_str) for name in names]
 
 
-def add_suffix(names: list[str], suffix: str) -> list[str]:
+def add_suffix(names: Names, suffix: str) -> Names:
     def add_to_base(name: str) -> str:
         base, sep, ext = name.rpartition('.')
         return f'{base}{suffix}.{ext}' if sep else f'{name}{suffix}'
     return [add_to_base(name) for name in names]
 
 
-def clean_prefix(names: list[str]) -> list[str]:
+def clean_prefix(names: Names) -> Names:
     def remove_prefix(name: str) -> str:
         for sep in ['_', '-', ' ']:
             prefix, sep_found, remainder = name.partition(sep)
@@ -22,7 +25,7 @@ def clean_prefix(names: list[str]) -> list[str]:
     return [remove_prefix(name) for name in names]
 
 
-def reorder_names(names: list, separator: str) -> list[str]:
+def reorder_names(names: Names, separator: str) -> Names:
     names = clean_prefix(names)
     num_name = len(names)
     decimal = floor(log10(num_name)) + 1
@@ -32,7 +35,7 @@ def reorder_names(names: list, separator: str) -> list[str]:
     ]
 
 
-def get_duplicate_indices(names: list) -> list[int]:
+def get_duplicate_indices(names: Names) -> Indices:
     for idx1, name1 in enumerate(names):
         for idx2, name2 in enumerate(names[idx1+1:]):
             if name2 == name1:
--- utils/widget_control.py
+++ utils/widget_control.py
@@ -1,17 +1,20 @@
 import tkinter as tk
 
+type Names = list[str]
+type Indices = list[int]
+
 
 def toggle_widget_state(widget: tk.Widget, enable: bool):
     '''Enable or disable a widget based on the enable flag.'''
     widget.config(state='normal' if enable else 'disabled')
 
 
-def update_listbox_content(listbox: tk.Listbox, names: list):
+def update_listbox_content(listbox: tk.Listbox, names: Names):
     listbox.delete(0, tk.END)
     for idx, name in enumerate(names):
         listbox.insert(idx, name)
 
 
-def highlight_duplicates(listbox: tk.Listbox, indices: list, bg_color: str = 'red'):
+def highlight_duplicates(listbox: tk.Listbox, indices: Indices, bg_color: str = 'red'):
     for idx in indices:
         listbox.itemconfig(idx, {'bg': bg_color})
